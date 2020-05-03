112923d4eee4dfbe5f9dae4e9645961e72aa4eb9
Luiza Natasha <luyzanatashaxd@gmail.com>
2020-05-03 11:36:06 -0400

    parte1 commit

--- app/src/main/java/com/example/wayless/MainActivity.java
+++ app/src/main/java/com/example/wayless/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.wayless;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn01, btn02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        btn01 = (Button) findViewById(R.id.btn);
+        btn02 = (Button) findViewById(R.id.btn2);
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btn01Activity();
+            }
+        });
+
+    }
+    private  void btn01Activity(){
+        startActivity(new Intent(MainActivity.this, Tela_login.class));
     }
 }
+
---
+++ app/src/main/java/com/example/wayless/Tela_login.java
@@ -0,0 +1,15 @@
+package com.example.wayless;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+        getSupportActionBar().hide();
+    }
+}
